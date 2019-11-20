6a1810c37780278af6040105f06f565b12da6e3b
Richard Clark <richard@depop.com>
2019-11-20 07:02:31 +0000

    Refactor python-default

--- service/python/default/app.py
+++ service/python/default/app.py
@@ -1,37 +1,65 @@
 #!/usr/bin/env python
 import os
 import json
+from enum import IntEnum, unique
 
 from flask import Flask, Response
 from datadog import DogStatsd
+from pydantic.dataclasses import dataclass
 
-config = {
-    "gender": "MALE",
-    "metrics_host": os.getenv('METRICS_HOSTNAME', 'localhost'),
-    "hostname": os.environ['HOSTNAME'],
-    "env_name": os.environ['ENV_NAME'],
-}
 
-statsd = DogStatsd(host=config["metrics_host"])
+@unique
+class Sex(IntEnum):
+    MALE = 0
+    FEMALE = 1
+    OTHER = 2
+
+
+@dataclass
+class Config:
+    sex: Sex
+    metrics_host: str
+    env_name: str
+    hostname: str
+
+
+class ConfigEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Config):
+            skip_keys = ('__initialised__', 'sex')
+            overrides = {'sex': obj.sex.name}
+            main_dict = {k: v for k, v in obj.__dict__.items() if k not in skip_keys}
+            return {**overrides, **main_dict}
+        return json.JSONEncoder.default(self, obj)
+
+
+config = Config(
+    sex=getattr(Sex, os.environ['SEX']),
+    metrics_host=(os.getenv('METRICS_HOSTNAME', 'localhost')),
+    env_name=os.environ['ENV_NAME'],
+    hostname=os.environ['HOSTNAME'],
+)
+
+statsd = DogStatsd(host=config.metrics_host)
 statsd.constant_tags = [
-    f'gender:{config["gender"]}',
-    f'hostname:{config["hostname"]}',
-    f'env_name:{config["env_name"]}',
+    f'sex:{config.sex.name}',
+    f'hostname:{config.hostname}',
+    f'env_name:{config.env_name}',
 ]
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def get_index():
+@app.route('/config', methods=['GET'])
+def get_config():
     response = Response(
-        json.dumps(config, indent=2),
+        json.dumps(config, cls=ConfigEncoder, indent=2),
         status=200,
         mimetype='application/json'
     )
     statsd.increment('success', tags=[
         'method:GET',
-        'function:get_index',
+        'function:get_config',
         f'status_code:{response.status_code}',
     ])
     return response
